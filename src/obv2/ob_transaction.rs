12ee52a6d0475d6f80a4cc652af8706ba1064810
gigazh <zhmolly@gigadao.io>
2024-06-07 00:07:09 +0900

    feat: modify transaction log parser

--- src/obv2/ob_transaction.rs
+++ src/obv2/ob_transaction.rs
@@ -1,20 +1,12 @@
-use anchor_lang::{AnchorDeserialize, Discriminator};
+use anchor_lang::Discriminator;
+use base64::{prelude::BASE64_STANDARD, Engine};
 use bytemuck::{self, cast_ref};
-use openbook_v2::instruction::{
-    CancelAllAndPlaceOrders, CancelOrder, ConsumeEvents, PlaceOrder, PlaceOrders, PlaceTakeOrder,
-    SettleFunds,
-};
-use std::mem;
-use std::str::FromStr;
-use std::time::{SystemTime, UNIX_EPOCH};
+use openbook_v2::logs::{FillLog, OpenOrdersPositionLog, SettleFundsLog};
+use openbook_v2::state::FillEvent;
 
 use crate::structs::{Account, BotMsg, MessageTransaction, ObV2Cancel, ObV2Event, ObV2Fill};
-use crate::utils::{is_buy, token_decimals};
 use crate::Parser;
-use anchor_lang::prelude::Pubkey;
 use async_trait::async_trait;
-use openbook_v2::state::{BookSide, EventHeap, EventType, FillEvent, OutEvent, Side};
-use solana_client::nonblocking::rpc_client::RpcClient;
 
 #[derive(Clone, Debug)]
 pub struct ObV2TransactionsPlugin {
@@ -52,6 +44,8 @@ impl Parser for ObV2TransactionsPlugin {
 
         tracing::info!("tx: {}, slot: {}", transaction.signature, slot);
 
+        // No need to parse instructions (because we need generated events)
+        /*
         // Loop instructions
         for (index, ix) in transaction.instructions.iter().enumerate() {
             if ix.program_id.eq(&self.program_id) && ix.accounts.contains(&account_idx) {
@@ -59,18 +53,24 @@ impl Parser for ObV2TransactionsPlugin {
                 let discriminator = &ix.data[0..8];
                 if discriminator == CancelOrder::DISCRIMINATOR {
                     tracing::info!("cancel order");
+
+                    let tx = CancelOrder::deserialize(&mut &ix.data[8..])?;
                 } else if discriminator == CancelAllAndPlaceOrders::DISCRIMINATOR {
                     tracing::info!("cancel & place orders");
 
                     let tx = CancelAllAndPlaceOrders::deserialize(&mut &ix.data[8..])?;
-                    tracing::info!("bids: {:?}", tx.bids);
-                    tracing::info!("asks: {:?}", tx.asks);
                 } else if discriminator == PlaceOrder::DISCRIMINATOR {
                     tracing::info!("place order");
+
+                    let tx = PlaceOrder::deserialize(&mut &ix.data[8..])?;
                 } else if discriminator == PlaceTakeOrder::DISCRIMINATOR {
                     tracing::info!("place take order");
+
+                    let tx = PlaceTakeOrder::deserialize(&mut &ix.data[8..])?;
                 } else if discriminator == PlaceOrders::DISCRIMINATOR {
                     tracing::info!("place multiple orders");
+
+                    let tx = PlaceOrders::deserialize(&mut &ix.data[8..])?;
                 } else if discriminator == SettleFunds::DISCRIMINATOR {
                     tracing::info!("settle funds");
                 } else if discriminator == ConsumeEvents::DISCRIMINATOR {
@@ -80,7 +80,45 @@ impl Parser for ObV2TransactionsPlugin {
                 }
             }
         }
+        */
 
+        // Check logs
+        let mut start_idx: i16 = -1;
+        for (idx, log) in transaction.logs.iter().enumerate() {
+            // check openbookv2 events only
+            if log.eq(&format!("Program {} invoke [1]", self.program_id)) {
+                start_idx = idx as i16;
+                continue;
+            }
+
+            // if openbookv2's ix finished
+            if log.eq(&format!("Program {} success", self.program_id)) {
+                start_idx = -1;
+                continue;
+            }
+
+            // if in openbookv2 instruction
+            if start_idx > -1 && log.starts_with("Program data:") {
+                match BASE64_STANDARD.decode(&log[14..]) {
+                    Ok(data) => {
+                        // Check event type
+                        let discriminator = &data[0..8];
+                        if discriminator == FillLog::DISCRIMINATOR {
+                            tracing::info!("fill order");
+                        } else if discriminator == OpenOrdersPositionLog::DISCRIMINATOR {
+                            tracing::info!("positions order");
+                        } else if discriminator == SettleFundsLog::DISCRIMINATOR {
+                            // tracing::info!("settle funds");
+                        } else {
+                            tracing::info!("other event: {:?}", discriminator);
+                        }
+                    }
+                    Err(_) => {
+                        tracing::error!("Parse obv2 data error");
+                    }
+                }
+            }
+        }
         Ok(BotMsg::Unimplemented)
     }
 }
